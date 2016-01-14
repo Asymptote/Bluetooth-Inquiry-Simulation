902aad745963e26bc531b84c42c157581c6d708c
Ahsan Shafiq Chaudhry <ahsan.shafiq786@gmail.com>
2016-01-14 10:04:22 +0300

    Added Inquiry Protocol and some basic analysis of the output. Not entirely working as expected but it is close.

--- Inquiry.py
+++ Inquiry.py
@@ -1,81 +1,108 @@
 import Operations as op
 
-kOffset_a = 24
-kOffset_b = 8
+def run():
 
-clk = 0
-arr = []
+	kOffset_a = 24
+	kOffset_b = 8
 
-isNextTrainA = True
+	clk = 0
+	arr = []
 
-for clk in range(8192 * 5):
+	isNextTrainA = True
 
-	if int(clk%(2*4096)) == 0:
-		if isNextTrainA:
-			offset = kOffset_a
-			isNextTrainA = False
+	offset = kOffset_a
+
+	for clk in range(0, 8192*4):
+		if int(clk%(8192)) == 0:
+			if isNextTrainA:
+				offset = kOffset_a
+				isNextTrainA = False
+			else:
+				offset = kOffset_b
+				isNextTrainA = True
+		
+		
+		
+
+		#The Bluetooth clock is a 28-bit counter with a clock cycle of 312.5µs.
+		# Bluetooth communication at the Baseband level is organised into time slots.
+		# Each slot has a length of two clock cycles, i.e. 625µs.
+		clk_bits = op.dec2bin(clk, 32) # change clock to 32 bits 
+		
+		#---------------------------
+		# 			Find X
+
+		# 0 index is right most one
+		# 4-0 would actually mean -> indices 27, 28, 29 , 30, 31
+
+		# Similarly take clock's 16-12
+		# +1 because python does not include b if a:b
+		clk_16_12 = op.bin2dec(clk_bits[31-16:31-12+1])
+		clk_4_2_0 = op.bin2dec(clk_bits[31-4:31-2+1]+clk_bits[31-0])
+
+
+		#################################################
+		sub_clk = 0
+		'''if clk_16_12 > clk_4_2_0:
+			sub_clk = clk_16_12 - clk_4_2_0
 		else:
-			offset = kOffset_b
-			isNextTrainA = True
-	
-	clk_bits = op.dec2bin(clk, 32) # change clock to 32 bits 
-
-	#---------------------------
-	# 			Find X
-
-	# 0 index is right most one
-	# 4-0 would actually mean -> indices 27, 28, 29 , 30, 31
-
-	# Similarly take clock's 16-12
-	# +1 because python does not include b if a:b
-	clk_16_12 = op.bin2dec(clk_bits[31-16:31-12+1])
-	clk_4_2_0 = op.bin2dec(clk_bits[31-4:31-2+1]+clk_bits[31-0])
-
-	sub_clk = 0
-	if clk_16_12 > clk_4_2_0:
-		sub_clk = clk_16_12 - clk_4_2_0
-	else:
-		sub_clk = clk_4_2_0 - clk_16_12
-	
-	
-	temp_clk = (sub_clk % 16) # in decimal
-	
-	Xi = op.dec2bin((clk_16_12 + offset + temp_clk) % 32, 32)
-	
-	X = Xi[31-4:31-0+1]
-	#print("X:" + X)
-	#---------------------------
-
-	# 0000 1001 1110 1000 
-	#address = [ 4-LSBs-of-DCI(0x00), GIAC LAP (0x9E8B33)]
-	address = op.hex2bin('09E8B33') # this return 32 bits
-
-	###########################
-	## we can flip the address
-
-	address = address[4:] # Get 28 bits
-
-	#print("Address:" + address)
-
-	A = address[27-27:27-23+1]
-	B = address[27-22:27-19+1]
-	C = ''.join((address[27-8],address[27-6],address[27-4],address[27-2],address[27-0]))
-	D = address[27-18:27-10+1]
-	E = ''.join((address[27-13],address[27-11],address[27-9],address[27-7],address[27-5],address[27-3],address[27-1]))
-	F = '0'
-	Y1 = clk_bits[31-1] * 5 # in binary
-	Y2 = op.dec2bin(32 * op.bin2dec(clk_bits[31-1]), 32) # in binary
-	#Y1 = '0'
-	#Y2 = '0'
+			sub_clk = clk_4_2_0 - clk_16_12
+		'''
+		# clk_4_2_0 is always a pattern like 0101 2323 ... 12131213, 14151415. (32 tics)
+		# clk_16_12 is 0 for 4096 tics and 1 for next 4096 then 2 so on
+		# subtraction doesn't make sense
+		sub_clk = (clk_4_2_0 - clk_16_12)		
+		temp_clk = (sub_clk % 16) # in decimal
+		
+		Xi = op.dec2bin((clk_16_12 + offset + temp_clk) % 32, 32)
+		X = Xi[31-4:31-0+1]
+		#print(">>> " + X)
+		#print("X: "+str(temp_clk))
+		#---------------------------
 
 
 
-	add_1 = '{0:05b}'.format((op.bin2dec(A) + op.bin2dec(X))%32)
-	xor_1 = '{0:05b}'.format(op.bin2dec(add_1) ^ op.bin2dec(B))
-	xor_2 = '{0:05b}'.format(op.bin2dec(C) ^ op.bin2dec(Y1))
-	perm_1 = op.perm5(xor_1, xor_2, D)
-	add_2 = op.bin_add(E,F,perm_1, Y2)
-	freq = op.bin2dec(add_2) % 79
-	arr.append(freq)
+
+
+		# 0000 1001 1110 1000 
+		#address = [ 4-LSBs-of-DCI(0x00), GIAC LAP (0x9E8B33)]
+		address = op.hex2bin('09E8B33') # this return 32 bits
+
+		###########################
+		## we can flip the address
+
+		address = address[4:] # Get 28 bits
+
+		#print("Address:" + address)
+
+		A = address[27-27:27-23+1]
+		B = address[27-22:27-19+1]
+		C = ''.join((address[27-8],address[27-6],address[27-4],address[27-2],address[27-0]))
+		D = address[27-18:27-10+1]
+		E = ''.join((address[27-13],address[27-11],address[27-9],address[27-7],address[27-5],address[27-3],address[27-1]))
+		F = '0'
+		
+		#print('clock [1]: ' + clk_bits[31-1])
+		Y1 = clk_bits[31-1] * 5 # in binary
+		#Y1 = clk_bits[31-2]
+
+		if clk_bits[31-1] == '0':
+			Y2 = '000000'
+		else:
+			Y2 = '100000'
+		#Y2 will either be 100000 or 000000
+		#Y2 = op.dec2bin(32 * op.bin2dec(clk_bits[31-1]), 32) # in binary
+
+		#Y1 = '0'
+		Y2 = '0'
+
+		add_1 = '{0:05b}'.format((op.bin2dec(A) + op.bin2dec(X))%32)
+		xor_1 = '{0:05b}'.format(op.bin2dec(add_1) ^ op.bin2dec(B))
+		xor_2 = '{0:05b}'.format(op.bin2dec(C) ^ op.bin2dec(Y1))
+		perm_1 = op.perm5(xor_1, xor_2, D)
+		add_2 = op.bin_add(E,F,perm_1, Y2)
+		freq = op.bin2dec(add_2) % 79
+		arr.append(freq)
+	return arr
 
 
--- Run.py
+++ Run.py
@@ -4,6 +4,15 @@ import pandas as pd
 import numpy as np
 from collections import defaultdict
 
+
+def print_f_count(freq, train):
+	freq = freq.tolist()
+	print("\n-------"+train+"--------")
+	print("Total Frequqncies: "+str(pd.unique(freq)))	
+	for i in pd.unique(freq):
+		print(str(i)+": "+str(freq.count(i)))
+
+'''
 #Slave testing
 freqSeq = []
 
@@ -39,10 +48,46 @@ odd = list(range(1,79,2))
 addresses = np.asarray((even+odd))
 
 print(addresses[indices])
-
-
-
+'''
 
 #Master testing
-#print("Running Master")
-#arr = master.run()
+print("Running Master")
+arr = master.run()
+
+freqSeq = arr
+
+indices = pd.unique(freqSeq)
+indices.sort()
+indices = indices.tolist()
+
+even = list(range(0,79,2))
+odd = list(range(1,79,2))
+addresses = np.asarray((even+odd))
+
+
+############################################
+
+ta_1 = arr[0:8192]
+tb_1 = arr[8192:2*8192]
+ta_2 = arr[2*8192:3*8192]
+tb_2 = arr[3*8192:4*8192]
+
+ta_1_f = addresses[ta_1]
+tb_1_f = addresses[tb_1]
+ta_2_f = addresses[ta_2]
+tb_2_f = addresses[tb_2]
+
+print("Train A-1 Unique Freqs: " + str(pd.unique(ta_1_f)))
+print("Train B-1 Unique Freqs: " + str(pd.unique(tb_1_f)))
+print("Train A-2 Unique Freqs: " + str(pd.unique(ta_2_f)))
+print("Train B-2 Unique Freqs: " + str(pd.unique(tb_2_f)))
+
+print_f_count(ta_1_f, 'Train A-1')
+print_f_count(tb_1_f, 'Train B-1')
+print_f_count(ta_2_f, 'Train A-2')
+print_f_count(tb_2_f, 'Train B-2')
+
+f_vals = addresses[indices]
+print("Values: " + str(f_vals))
+print("Total freqs: " + str(len(f_vals)))
+
